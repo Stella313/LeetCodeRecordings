e1899c2e46e39553aa25af6c6847725229f57001
Stella313 <silverdew313@gmail.com>
2021-07-27 02:21:02 +0800

    Update No_0006

--- src/No_0006.java
+++ src/No_0006.java
@@ -1,23 +1,32 @@
 public class No_0006 {
     public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() == 1) {
+            return s;
+        }
         String [] str = s.split("");
         int count = 0;
         String result = "";
-        String [][] matrix = new String[numRows][str.length / 2];
-        for(int j = 0;j < matrix[0].length - 1;j ++){
+        String [][] matrix = new String[numRows][str.length];
+        for(int j = 0;j < matrix[0].length;j ++){
             if(j % (numRows - 1) == 0){
                 for(int i = 0;i < numRows;i ++){
+                    if(count >= str.length)
+                        break;
                     matrix[i][j] = str[count];
                     count ++;
                 }
             }else{
-                matrix[numRows - 1 - j][j] = str[count];
+                if(count >= str.length)
+                    break;
+                matrix[numRows - 1 - j % (numRows - 1)][j] = str[count];
                 count ++;
             }
         }
         for(int i = 0;i < matrix.length;i ++){
             for(int j = 0;j < matrix[0].length;j ++){
-                result += matrix[i][j];
+                if(matrix[i][j] != null){
+                    result += matrix[i][j];
+                }
             }
         }
         return result;
